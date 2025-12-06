36d124ba083d4577f75741f9a8ee48ca870ea50e
aton-of-data <dornfeld.in@gmail.com>
2025-12-06 14:10:01 -0300

    chore: add project configuration files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@core': './src/core',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@store': './src/store',
+          '@domain': './src/types',
+          '@navigation': './src/navigation',
+          '@hooks': './src/hooks',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   preset: 'react-native',
+  moduleNameMapper: {
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@screens/(.*)$': '<rootDir>/src/screens/$1',
+    '^@store$': '<rootDir>/src/store',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+    '^@domain/(.*)$': '<rootDir>/src/types/$1',
+    '^@navigation$': '<rootDir>/src/navigation',
+    '^@navigation/(.*)$': '<rootDir>/src/navigation/$1',
+    '^@hooks$': '<rootDir>/src/hooks',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@reduxjs|react-redux)/)',
+  ],
 };
